bf0b2e72dc0d30d0505fd4a43a64b4d484f2fb33
UziXitado <20243ds096@utez.edu.mx>
2025-12-07 19:25:43 -0600

    Aqui representa una canción ya sea local o de red

--- app/src/main/java/com/example/integradorasensorproximidad/data/model/Song.kt
+++ app/src/main/java/com/example/integradorasensorproximidad/data/model/Song.kt
@@ -3,12 +3,21 @@ package com.example.integradorasensorproximidad.data.model
 import android.net.Uri
 
 /**
- * Representa un único archivo de audio encontrado en el dispositivo del usuario.
+ * Representa una canción, que puede ser local o de red.
  */
 data class Song(
     val id: Long,
     val title: String,
     val artist: String,
-    val duration: Long, // en milisegundos
-    val contentUri: Uri // La 'dirección' para acceder al archivo
-)
+    val duration: Long,
+
+    // Para canciones locales, esta será la URI del Content Provider.
+    val contentUri: Uri? = null,
+
+    // Para canciones de red, esta será la URL completa para el streaming.
+    val networkUrl: String? = null
+) {
+    // Propiedad para saber fácilmente si la canción es de red o no.
+    val isFromNetwork: Boolean
+        get() = networkUrl != null
+}
