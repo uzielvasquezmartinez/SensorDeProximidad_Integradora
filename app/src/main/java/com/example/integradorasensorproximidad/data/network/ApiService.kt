0bebf4905411ba7e1809333b18a248314fb826b0
UziXitado <20243ds096@utez.edu.mx>
2025-12-07 19:22:53 -0600

    Agregue nuevos metodos para el put y el delete

--- app/src/main/java/com/example/integradorasensorproximidad/data/network/ApiService.kt
+++ app/src/main/java/com/example/integradorasensorproximidad/data/network/ApiService.kt
@@ -1,32 +1,43 @@
 package com.example.integradorasensorproximidad.data.network
 
+import com.example.integradorasensorproximidad.data.model.NetworkSong
 import com.example.integradorasensorproximidad.data.model.Playlist
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.*
 
-/**
- * Interfaz que define los endpoints de la API para el CRUD de Playlists.
- */
 interface ApiService {
+    // --- Canciones ---
 
-    // CREATE
-    @POST("playlists")
-    suspend fun createPlaylist(@Body playlist: Playlist): Response<Playlist>
+    @GET("api/songs")
+    suspend fun getSongs(): List<NetworkSong>
 
-    // READ (all)
-    @GET("playlists")
-    suspend fun getAllPlaylists(): Response<List<Playlist>>
+    @Multipart
+    @POST("api/songs")
+    suspend fun uploadSong(
+        @Part("title") title: RequestBody,
+        @Part("artist") artist: RequestBody,
+        @Part("duration") duration: RequestBody,
+        @Part file: MultipartBody.Part
+    ): Response<NetworkSong>
 
-    // READ (one by id)
-    @GET("playlists/{id}")
+    // --- Playlists ---
 
-    suspend fun getPlaylistById(@Path("id") id: Int): Response<Playlist>
-    // UPDATE
-    @PUT("playlists/{id}")
-    suspend fun updatePlaylist(@Path("id") id: Int, @Body playlist: Playlist): Response<Playlist>
+    @GET("api/playlists")
+    suspend fun getPlaylists(): List<Playlist>
 
-    // DELETE
-    @DELETE("playlists/{id}")
-    suspend fun deletePlaylist(@Path("id") id: Int): Response<Unit>
+    @POST("api/playlists")
+    suspend fun createPlaylist(@Body playlistData: Playlist): Response<Playlist>
+    @PUT("api/playlists/{id}")
+    suspend fun updatePlaylist(
+        @Path("id") id: Int,
+        @Body playlistData: Playlist
+    ): Response<Playlist>
+
+    @DELETE("api/playlists/{id}")
+    suspend fun deletePlaylist(
+        @Path("id") id: Int
+    ): Response<Unit>
 
 }
